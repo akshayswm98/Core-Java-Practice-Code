62bd517a990b5c7d373a7a52b9cfbffcf24be51a
8600913764 <patilakshay8649@gmail.com>
2026-03-02 19:59:35 +0530

    first-commit

---
+++ A_SETTER_GETTER/src/akshay/gettersetter/EmployeeDemo.java
@@ -0,0 +1,72 @@
+package akshay.gettersetter;
+
+import java.util.Scanner;
+
+class Employee
+{
+	private String name;
+	private double salary;
+	public Employee(String name, double salary) 
+	{
+		if(name == null && salary < 0)
+		{
+			System.err.println("Error");
+			System.exit(0);
+		}
+		this.name = name;
+		this.salary = salary;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public double getSalary() 
+	{
+		return salary;
+	}
+	public void setSalary(double salary) 
+	{
+		if(salary > this.salary)
+		{
+			this.salary = salary;
+		}
+		else
+		{
+			System.err.println("Negative Increament Error");
+			System.exit(0);
+		}
+	}
+	@Override
+	public String toString() 
+	{
+		return "Employee [name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
+public class EmployeeDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Employee Name := ");
+		String name = sc.nextLine();
+		
+		System.out.println("Enter The Employee Salary:= ");
+		double salary = sc.nextDouble();
+		
+		Employee raj = new Employee(name, salary);
+		System.out.println(raj);
+		
+		System.out.println("Enter The Increament Amount := ");
+		double increament = sc.nextDouble();
+		
+		raj.setSalary(raj.getSalary()+increament);
+		System.out.println(raj);
+	}
+
+}
---
+++ A_SETTER_GETTER/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SETTER_GETTER {
+}
