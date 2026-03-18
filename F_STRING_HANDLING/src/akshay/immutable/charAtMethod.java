ba0dcb923e553d1d3559fc1dc99aafa07ebd6855
8600913764 <patilakshay8649@gmail.com>
2026-03-18 11:13:14 +0530

    commit

--- F_STRING_HANDLING/src/akshay/immutable/charAtMethod.java
+++ F_STRING_HANDLING/src/akshay/immutable/charAtMethod.java
@@ -5,6 +5,7 @@ public class charAtMethod
 
 	public static void main(String[] args) 
 	{
+		System.out.println("charAtMethod.main()");
 		String name = "AkshayPatil";
 		System.out.println(name.charAt(5)); // method are use to access particular character from the string
 		System.out.println(name.charAt(3));//h
