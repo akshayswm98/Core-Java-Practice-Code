c83fe2759e71edcce5e5e6dba0f2f1b069f77ba1
8600913764 <patilakshay8649@gmail.com>
2026-03-21 12:27:56 +0530

    commit

--- F_STRING_HANDLING/src/akshay/immutable/PalindromeString.java
+++ F_STRING_HANDLING/src/akshay/immutable/PalindromeString.java
@@ -6,22 +6,22 @@ public class PalindromeString
 {
 
 	public static void main(String[] args) 
-	{	String re = "";
+	{	String reverse = "";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Name :- ");
 		String name = sc.nextLine().toUpperCase();
 		
-		int s = name.length();
+		int length = name.length();
 		System.out.println("My String Name Is:- "+name);
-		System.out.println("My string Lenth Is :- "+s);
+		System.out.println("My string Lenth Is :- "+length);
 		
-		for(int i = s - 1; i>=0; i--)
+		for(int i = length - 1; i>=0; i--)
 		{
-			re = re + name.charAt(i);
+			reverse = reverse + name.charAt(i);
 		}
-		System.out.println("My Reverse String Is :-"+re);
+		System.out.println("My Reverse String Is :-"+reverse);
 		
-		if(name.equals(re))
+		if(name.equals(reverse))
 		{
 			System.out.println("is a palindrom");
 		}
