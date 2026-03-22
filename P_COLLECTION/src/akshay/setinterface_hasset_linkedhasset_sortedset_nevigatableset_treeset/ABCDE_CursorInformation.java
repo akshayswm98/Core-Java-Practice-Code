0ac7752d62fc1190d5ee1a1300cf7e2028a33d9d
8600913764 <patilakshay8649@gmail.com>
2026-03-22 14:13:12 +0530

    commit

--- P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/ABCDE_CursorInformation.java
+++ P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/ABCDE_CursorInformation.java
@@ -1,4 +1,4 @@
-package akshay.queinterface;
+package akshay.setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset;
 public class ABCDE_CursorInformation {}
 /*
  * Generally cursor we can use for fetch the elements one by one 
