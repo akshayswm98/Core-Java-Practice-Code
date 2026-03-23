6a408673b4eefd87b6ffc44ede1de1a065f2dc8d
8600913764 <patilakshay8649@gmail.com>
2026-03-23 13:50:35 +0530

    commit

---
+++ P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/HashSetDemo5.java
@@ -0,0 +1,22 @@
+package akshay.setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset;
+
+import java.util.HashSet;
+
+public class HashSetDemo5 
+{
+	public static void main(String[] args) 
+	{
+		HashSet hs = new HashSet();
+		hs.add("Ravi");
+		hs.add("Vijay");
+		hs.add("Ravi");
+		hs.add("Ajay");
+		hs.add(null);
+		hs.add("Sweta");
+		hs.add(null);
+		
+		hs.forEach(System.out::println);
+    }
+}
+
+/*print Randomly and not printed multiple null value and not allowed duplicates.*/
