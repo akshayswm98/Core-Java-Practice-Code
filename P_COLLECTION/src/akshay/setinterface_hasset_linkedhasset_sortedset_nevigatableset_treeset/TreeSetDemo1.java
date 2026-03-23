aa0f4a79e421e09bb1efa6e14e2a1999a3037ad3
8600913764 <patilakshay8649@gmail.com>
2026-03-23 17:20:04 +0530

    commit

---
+++ P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/TreeSetDemo1.java
@@ -0,0 +1,18 @@
+package akshay.setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo1 
+{
+	public static void main(String[] args) 
+	{
+		TreeSet ts = new TreeSet();
+		ts.add(40);
+		ts.add(30);
+		ts.add(20);
+		ts.add(10);
+		ts.add(50);
+		System.out.println(ts);
+	}
+}
+/*It is follow sorting not order but follow insertion order*/
