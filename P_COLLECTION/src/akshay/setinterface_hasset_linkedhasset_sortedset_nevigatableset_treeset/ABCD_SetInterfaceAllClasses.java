c2fc5dd0c0bd569100aad1023f23742f617441eb
8600913764 <patilakshay8649@gmail.com>
2026-03-23 15:31:25 +0530

    commit

--- P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/ABCD_SetInterfaceAllClasses.java
+++ P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/ABCD_SetInterfaceAllClasses.java
@@ -3,14 +3,13 @@ package akshay.setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset
 public class ABCD_SetInterfaceAllClasses {
 	
 	/*
-	 Set<E>interface:-
+	 Set<E>interface:-[UNORDER,UNSORTED,NO DUPLICATES]
 	 ===============
 	 * Set is implemented Interface from collection interface.
 	 * set interface available in java.util package from JDK 1.2v.
 	 * set interface does not follow indexing technique, 
 	   actually it is use "hashing technique".
 	 * set interface never accept duplicate elements.
-	 * so set interface store only one null value. 
 	 * So ListItorator interface does will not work with set<E> interface.
 	 * set<E> are use all the method are collection<E> interface, 
 	   few more method are added from JDK 9v.
@@ -23,8 +22,34 @@ public class ABCD_SetInterfaceAllClasses {
 	                                        TreeSet(c)1.2v
 	                                        
 	                                        
-	                                        
+	 
+	 1) HashSet <E>:-
+	 ================
+	 * It is implemented class from set interface available in JDK 1.2v.
+	 * It does not accept duplicates multiple null values.
+	 * It  can accept homogeneous and heterogeneous value and one null value.
+	 * It is un_order and un_sorted set.
+	 * It is use hash table data structure.
+	 * The elements are inserted base on the hash code.
+	 * Default capacity is 16.
+	 * Methods are not synchronize so thread are not secure.
+	 * It is mainly used of to search elements of large amount of data. 
+	 
+	  
+	 Constructor:-
+	 =============
+	 1) HashSet<E> hs = new HashSet():-
+	 * It will create the HashSet object default capacity is 16. 
+	 
+	 2) HashSet<E> hs = new HashSet(Int initial capacity):-
+	 * Use specified capacity.
+	 
+	 3) HashSet<E> hs = new HashSet(Int initial capacity,Float loadFactor):-
+	 * We can specified our own initial capacity and load factor.
+	 
+	 4)  HashSet<E> hs = new HashSet(Collection c):-
+	 * Interconversion of collection
 	 */
 	
-
+	
 }
---
+++ P_COLLECTION/src/akshay/setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset/HashSetDemo7CRUDOperation.java
@@ -0,0 +1,131 @@
+package akshay.setinterface_hasset_linkedhasset_sortedset_nevigatableset_treeset;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSetDemo7CRUDOperation 
+{
+	public static void main(String[] args) 
+	{
+		HashSet<String> hs = new HashSet<String>();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("Option");
+			System.out.println("1.Add Elements");
+			System.out.println("2.Delete Elements");
+			System.out.println("3.Display Elements");
+			System.out.println("4.Serach Elements");
+			System.out.println("5.Update Elements");
+			System.out.println("6.Exit");
+			
+			System.out.println("Enter Your Choice(1/2/3/4/5/6)");
+			int choice = Integer.parseInt(sc.nextLine());
+			
+			switch(choice)
+			{
+			case 1:
+				System.out.println("Enter the elements to add:- ");
+				String addElement = sc.nextLine();
+				if(!addElement.matches("[a-zA-Z]+"))
+				{
+					System.err.println("Invalid Input! Only alphabets are allowed.");
+					break;
+				}
+				if(hs.add(addElement))
+				{
+					System.out.println(addElement + " Element Added Succesfully.");
+				}
+				else
+				{
+					System.err.println(addElement + " Elements Already Exists.");
+				}
+				break;
+				
+			case 2:
+				if(hs.isEmpty())
+				{
+					System.err.println("HashSet Is Empty..!");
+				}
+				else
+				{
+					System.out.println("Enter the element to delete");
+					String deleteElement = sc.nextLine();
+					if(hs.remove(deleteElement))
+					{
+						System.out.println(deleteElement + " Element Delete Succesfully");
+					}
+					else
+					{
+						System.err.println(deleteElement + " Elements Not Found");
+					}
+				}
+				break;
+				
+			case 3:
+				if(hs.isEmpty())
+				{
+					System.err.println("HashSet Is Empty");
+				}
+				else
+				{
+					System.out.println("HashSet Total Elements");
+					hs.forEach(System.out::println);
+				}
+				break;
+				
+			case 4:
+				System.out.println("Enter Elements To search");
+				String searchElement = sc.nextLine();
+				if(hs.contains(searchElement))
+				{
+					System.out.println(searchElement + " Have in List");
+				}
+				else
+				{
+					System.err.println(searchElement + " Not Available in list");
+				}
+				break;
+				
+			case 5:
+			    if (hs.isEmpty()) 
+			    {
+			        System.err.println("Set is Empty");
+			    } 
+			    else 
+			    {
+			        System.out.println("Enter element to update:");
+			        String oldElement = sc.nextLine();
+
+			        if (hs.contains(oldElement)) 
+			        {
+			            System.out.println("Enter new element:");
+			            String newElement = sc.nextLine();
+
+			            hs.remove(oldElement);
+			            hs.add(newElement);
+
+			            System.out.println("Element updated successfully");
+			        } 
+			        else 
+			        {
+			            System.out.println("Element not found");
+			        }
+			    }
+			    break;
+			    
+			case 6:
+				System.out.println("Existing the program");
+				sc.close();
+				System.exit(0);
+				
+				default:
+					System.out.println("Invalid choice");
+			    
+			    
+			}
+		}
+	}
+}
